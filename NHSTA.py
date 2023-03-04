5bad08e2138ec4394884aa34c4450a2ade1d935a
rajshinde <rajshinde126@gmail.com>
2023-03-04 23:27:02 +0530

    Update NHSTA.py

--- NHSTA.py
+++ NHSTA.py
@@ -12,7 +12,7 @@ output_data = ''
 #Read the input data which consists of VIN 10 numbers.
 #Remove the duplicates to save unnecessary API calls. Lastly, convert the data to list form
 
-data = pd.read_csv('C:/Users/rajsh/Downloads/Manheim_VIN_Data.csv')
+data = pd.read_csv('/Manheim_VIN_Data.csv')
 data.drop(data[data[['VIN 10']].duplicated()].index,axis=0,inplace=True)
 data_list = data['VIN 10'].tolist()
 
@@ -25,12 +25,12 @@ total_api_calls = math.ceil(len(data)/50)
 for x in range(0,total_api_calls):
     post_fields['data'] = ';'.join(data_list[50 * x : 50 * (x+1)])
     r = requests.post(url, data=post_fields);
-    with open("C:/Users/rajsh/Desktop/Output.csv","a") as text_File:
+    with open("/Output.csv","a") as text_File:
         text_File.write(r.text)
 
     
 
 # This block of code removes the header column present throughtout per 50 records and then removes the blank records also.
-df = pd.read_csv('C:/Users/rajsh/Desktop/Output.csv', encoding= 'unicode_escape')
+df = pd.read_csv('/Output.csv', encoding= 'unicode_escape')
 df = df[df.makeid != 'makeid']
-df.to_csv('C:/Users/rajsh/Desktop/Output.csv', index=False)
+df.to_csv('/Output.csv', index=False)
